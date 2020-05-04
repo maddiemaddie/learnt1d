157b77f686a43159148a7cce99fff471934faca9
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-04 18:58:15 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,9 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Sample website using Next.js</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website)
         </p>
       </section>
     </Layout>
