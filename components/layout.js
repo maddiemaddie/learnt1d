a1c8a4d51cde861140fa8b84efd6e22ed358aade
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-04 18:55:49 -0500

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Your Name'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Maddie T'
+export const siteTitle = 'Maddie Sample Website'
 
 export default function Layout({ children, home }) {
   return (
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="I'm learning how to build a site using Next.js"
         />
         <meta
           property="og:image"
