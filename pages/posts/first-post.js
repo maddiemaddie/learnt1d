b4e35d16bf16de28838cbab1e80e1a35fe1c36bf
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-04 19:59:38 -0500

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 export default function FirstPost() {
