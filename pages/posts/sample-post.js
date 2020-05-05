34b9dc74df6c6f8358f08850ed4cbb8df9449636
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-04 20:39:48 -0500

    Update sample-post.js

--- pages/posts/sample-post.js
+++ pages/posts/sample-post.js
@@ -1,17 +1,21 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-export default function FirstPost() {
+export default function SamplePost() {
   return (
     <>
       <Head>
-        <title>First Post</title>
+        <title>Sample Post</title>
       </Head>
-      <h1>First Post</h1>
+      <h1>Sample Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
+        <br>
+       <Link href="/posts/sample-post/">
+          <a>Sample post</a>
+      </Link>
       </h2>
     </>
   )
