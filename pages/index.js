82dbb814ad2fc96425cb8baa4f989bd3f5f5d771
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-05 19:46:30 -0500

    hmmmmmmm

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { siteTitle } from '../components/layout'
+import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
@@ -9,12 +9,10 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-  <section className={utilStyles.heading2X1}>
         <h2>Diabetes sucks.</h2>
         <p class="des">
         We're here to help. Please click which description best fits you.
         </p>
-    </section>
       <p><Link href="/posts/sample-post/"><a>Diabetic</a></Link> or <Link href="/posts/first-post/"><a>Supporter</a></Link></p>
     </Layout>
   )
