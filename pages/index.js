f97ad5e36a00ad5adf7c8a1f042b45626b468b00
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-05 19:24:34 -0500

    changing heading type

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,12 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-  //    <section className={utilStyles.headingMd}>
+  <section className={utilStyles.heading2X1}>
         <h2>Diabetes sucks.</h2>
         <p class="des">
         We're here to help. Please click which description best fits you.
         </p>
-    //  </section>
+    </section>
       <p><Link href="/posts/sample-post/"><a>Diabetic</a></Link> or <Link href="/posts/first-post/"><a>Supporter</a></Link></p>
     </Layout>
   )
