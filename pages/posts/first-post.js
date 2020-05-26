2eaceddf6ba988d0f6d8cff60d181b5a4a886d78
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-26 18:12:19 -0500

    chatbot

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,26 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import ChatBot from 'react-simple-chatbot'
 
 export default function FirstPost() {
   return (
-    <>
       <Head>
-        <title>First Post</title>
+        <title>{siteTitle}</title>
       </Head>
+      <ChatBot
+        steps={[
+          {
+            id: 'hello-world',
+            message: 'Hello World!',
+            end: true,
+          },
+        ]}
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
   )
 }
