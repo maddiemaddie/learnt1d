a27bba017ca2906388d160bb26a8960c8ca1348d
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-26 17:47:46 -0500

    global styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function FirstPost() {
   return (
@@ -13,14 +15,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-      <section class="text-gray-500 bg-gray-900 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
-      <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-white">Slow-carb next level shoinddgoitch ethical authentic, scenester sriracha forage.</h1>
-      <button class="flex-shrink-0 text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg mt-10 sm:mt-0">Button</button>
-    </div>
-  </div>
-</section>
     </>
   )
 }
