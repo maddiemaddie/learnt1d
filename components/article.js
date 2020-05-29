e65c91bb11be3567e1236787c00d83e16fd4f599
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-29 15:31:36 -0500

    adding article styling

---
+++ components/article.js
@@ -0,0 +1,105 @@
+function Article() {
+  return (
+    <>
+    <h3>What is type 1 diabetes?</h3>
+<p>Diabetes occurs when your <b>blood glucose</b>, also called blood sugar, is too high. Blood glucose is your main source of energy and comes mainly from the food you eat. <b>Insulin</b>, a hormone made by the pancreas, helps the glucose in your blood get into your cells to be used for energy. Another hormone, <b>glucagon</b>, works with insulin to control blood glucose levels.
+<br>
+In most people with type 1 diabetes, the body’s immune system, which normally fights infection, attacks and destroys the cells in the pancreas that make insulin. As a result, your pancreas stops making insulin. Without insulin, glucose can’t get into your cells and your blood glucose rises above normal. People with type 1 diabetes need to take insulin every day to stay alive.
+<br>
+People with type 1 diabetes need to take insulin every day.
+Who is more likely to develop type 1 diabetes?
+Type 1 diabetes typically occurs in children and young adults, although it can appear at any age. Having a parent or sibling with the disease may increase your chance of developing type 1 diabetes. In the United States, about 5 percent of people with diabetes have type 1.1
+<br>
+What are the symptoms of type 1 diabetes?
+Symptoms of type 1 diabetes are serious and usually happen quickly, over a few days to weeks. Symptoms can include
+<br>
+<ul>
+<li>increased thirst and urination</li>
+<li>increased hunger</li>
+<li>blurred vision</li>
+<li>fatigue</li>
+<li>unexplained weight loss</li>
+</ul>
+Sometimes the first symptoms of type 1 diabetes are signs of a life-threatening condition called diabetic ketoacidosis (DKA). Some symptoms of DKA include
+
+breath that smells fruity
+dry or flushed skin
+nausea or vomiting
+stomach pain
+trouble breathing
+trouble paying attention or feeling confused
+DKA is serious and dangerous. If you or your child have symptoms of DKA, contact your health care professional right away, or go to the nearest hospital emergency room.
+
+What causes type 1 diabetes?
+Experts think type 1 diabetes is caused by genes and factors in the environment, such as viruses, that might trigger the disease. Researchers are working to pinpoint the causes of type 1 diabetes through studies such as TrialNet.
+
+How do health care professionals diagnose type 1 diabetes?
+Health care professionals usually test people for type 1 diabetes if they have clear-cut diabetes symptoms. Health care professionals most often use the random plasma glucose (RPG) test to diagnose type 1 diabetes. This blood test measures your blood glucose level at a single point in time. Sometimes health professionals also use the A1C blood test to find out how long someone has had high blood glucose.
+
+Even though these tests can confirm that you have diabetes, they can’t identify what type you have. Treatment depends on the type of diabetes, so knowing whether you have type 1 or type 2 is important.
+
+To find out if your diabetes is type 1, your health care professional may test your blood for certain autoantibodies. Autoantibodies are antibodies that attack your healthy tissues and cells by mistake. The presence of certain types of autoantibodies is common in type 1 but not in type 2 diabetes.
+
+Because type 1 diabetes can run in families, your health care professional can test your family members for autoantibodies. Type 1 diabetes TrialNet, an international research network, also offers autoantibody testing to family members of people diagnosed with the disease. The presence of autoantibodies, even without diabetes symptoms, means the family member is more likely to develop type 1 diabetes. If you have a brother or sister, child, or parent with type 1 diabetes, you may want to get an autoantibody test. People age 20 or younger who have a cousin, aunt, uncle, niece, nephew, grandparent, or half-sibling with type 1 diabetes also may want to get tested.
+
+What medicines do I need to treat my type 1 diabetes?
+If you have type 1 diabetes, you must take insulin because your body no longer makes this hormone. Different types of insulin start to work at different speeds, and the effects of each last a different length of time. You may need to use more than one type. You can take insulin a number of ways. Common options include a needle and syringe, insulin pen, or insulin pump.
+
+Some people who have trouble reaching their blood glucose targets with insulin alone also might need to take another type of diabetes medicine that works with insulin, such as pramlintide. Pramlintide, given by injection, helps keep blood glucose levels from going too high after eating. Few people with type 1 diabetes take pramlintide, however. The NIH has recently funded a large research study to test use of pramlintide along with insulin and glucagon in people with type 1 diabetes. Another diabetes medicine, metformin, may help decrease the amount of insulin you need to take, but more studies are needed to confirm this. Reseachers are also studying other diabetes pills that people with type 1 diabetes might take along with insulin.
+
+Hypoglycemia, or low blood sugar, can occur if you take insulin but don’t match your dose with your food or physical activity. Severe hypoglycemia can be dangerous and needs to be treated right away. Learn more about hypoglycemia and how to prevent or treat it.
+
+How else can I manage type 1 diabetes?
+Along with insulin and any other medicines you use, you can manage your diabetes by taking care of yourself each day. Following your diabetes meal plan, being physically active, and checking your blood glucose often are some of the ways you can take care of yourself. Work with your health care team to come up with a diabetes care plan that works for you. If you are planning a pregnancy with diabetes, try to get your blood glucose levels in your target range before you get pregnant.
+
+Do I have other treatment options for my type 1 diabetes?
+The National Institute of Diabetes and Digestive and Kidney Diseases (NIDDK) has played an important role in developing “artificial pancreas” technology. An artificial pancreas replaces manual blood glucose testing and the use of insulin shots. A single system monitors blood glucose levels around the clock and provides insulin or a combination of insulin and glucagon automatically. The system can also be monitored remotely, for example by parents or medical staff.
+
+In 2016, the U.S. Food and Drug Administration approved a type of artificial pancreas system called a hybrid closed-loop system. This system tests your glucose level every 5 minutes throughout the day and night through a continuous glucose monitor, and automatically gives you the right amount of basal insulin, a long-acting insulin, through a separate insulin pump. You still need to manually adjust the amount of insulin the pump delivers at mealtimes and when you need a correction dose. You also will need to test your blood with a glucose meter several times a day. Talk with your health care provider about whether this system might be right for you.
+
+The illustration below shows the parts of a type of artificial pancreas system.
+
+An artificial pancreas system uses a continuous glucose monitor, an insulin pump, and a control algorithm to give you the right amount of basal insulin.
+The continuous glucose monitor sends information through a software program called a control algorithm. Based on your glucose level, the algorithm tells the insulin pump how much insulin to deliver. The software program could be installed on the pump or another device such as a cell phone or computer.
+
+Starting in late 2016 and early 2017, the NIDDK has funded several important studies on different types of artificial pancreas devices to better help people with type 1 diabetes manage their disease. The devices may also help people with type 2 diabetes and gestational diabetes.
+
+NIDDK is also supporting research into pancreatic islet transplantation—an experimental treatment for hard-to-control type 1 diabetes. Pancreatic islets are clusters of cells in the pancreas that make insulin. Type 1 diabetes attacks these cells. A pancreatic islet transplant replaces destroyed islets with new ones that make and release insulin. This procedure takes islets from the pancreas of an organ donor and transfers them to a person with type 1 diabetes. Because researchers are still studying pancreatic islet transplantation, the procedure is only available to people enrolled in a study. Learn more about islet transplantation studies.
+
+What health problems can people with type 1 diabetes develop?
+Over time, high blood glucose leads to problems such as
+
+heart disease
+stroke
+kidney disease
+eye problems
+dental disease
+nerve damage
+foot problems
+depression
+sleep apnea
+If you have type 1 diabetes, you can help prevent or delay the health problems of diabetes by managing your blood glucose, blood pressure, and cholesterol, and following your self-care plan.
+
+Can I lower my chance of developing type 1 diabetes?
+At this time, type 1 diabetes can’t be prevented. However, through studies such as TrialNet, researchers are working to identify possible ways to prevent or slow down the disease.
+
+<div class="references">
+References
+[1] Centers for Disease Control and Prevention. National diabetes statistics report, 2017. Centers for Disease Control and Prevention website. www.cdc.gov/diabetes/pdfs/data/statistics/national-diabetes-statistics-report.pdf (PDF, 1.3 MB) . Updated July, 18 2017. Accessed August 1, 2017.
+
+This content is provided as a service of the National Institute of Diabetes and Digestive and Kidney Diseases (NIDDK), part of the National Institutes of Health. The NIDDK translates and disseminates research findings to increase knowledge and understanding about health and disease among patients, health professionals, and the public. Content produced by the NIDDK is carefully reviewed by NIDDK scientists and other experts.
+
+The NIDDK would like to thank:
+Rita Basu, M.D., Mayo Clinic
+
+Syndicated Content Details:
+Source URL: https://www.niddk.nih.gov/health-information/diabetes/overview/what-is-diabetes/type-1-diabetes
+Source Agency: National Institute of Diabetes and Digestive and Kidney Diseases (NIDDK)
+Captured Date: 2018-04-17 16:33:34.0
+</div>
+</p>
+</>
+  )
+}
+
+export default Article
