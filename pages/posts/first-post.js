e60768c0c108212ecae1be2f75390f22019fdc3d
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-29 13:51:41 -0500

    content as js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,12 +8,12 @@ export default function FirstPost() {
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
-      <script>$(document).ready(function(){$.getJSON('https://api.digitalmedia.hhs.gov/api/v2/resources/media/18240/syndicate.json?stripStyles=true&stripScripts=true&stripBreaks=false&stripImages=true&stripClasses=true&stripIds=true&displayMethod=undefined&autoplay=false&userId=4673&callback=?', function(data){$('#syndicatedContent_18240_227960618084237').html(data.results[0].content);});});</script><div id="syndicatedContent_18240_227960618084237"></div>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
     </>
+    $(document).ready(function(){$.getJSON('https://api.digitalmedia.hhs.gov/api/v2/resources/media/18240/syndicate.json?stripStyles=true&stripScripts=true&stripBreaks=false&stripImages=true&stripClasses=true&stripIds=true&displayMethod=undefined&autoplay=false&userId=4673&callback=?', function(data){$('#syndicatedContent_18240_227960618084237').html(data.results[0].content);});});
   )
 }
