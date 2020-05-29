f63a173d13fef89c0aa6acaa4ef860f7f22b2734
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-29 13:42:12 -0500

    adding div

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,7 +8,9 @@ export default function FirstPost() {
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
+      <div class="content">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script><script>$(document).ready(function(){$.getJSON('https://api.digitalmedia.hhs.gov/api/v2/resources/media/18240/syndicate.json?stripStyles=true&stripScripts=true&stripBreaks=false&stripImages=true&stripClasses=true&stripIds=true&displayMethod=undefined&autoplay=false&userId=4673&callback=?', function(data){$('#syndicatedContent_18240_227266722363607').html(data.results[0].content);});});</script><div id="syndicatedContent_18240_227266722363607"></div>
+      </div>
       <h2>
         <Link href="/">
           <a>Back to home</a>
