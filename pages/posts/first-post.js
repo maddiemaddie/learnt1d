f0081e6066a8c4488b1640161bd7eb3f99b0d03b
madtharp <55772921+madtharp@users.noreply.github.com>
2020-05-29 13:57:16 -0500

    iframe

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,12 +8,12 @@ export default function FirstPost() {
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
+      <iframe src="https://api.digitalmedia.hhs.gov/api/v2/resources/media/18240/syndicate.html?stripStyles=true&stripScripts=true&stripBreaks=false&stripImages=true&stripClasses=true&stripIds=true&displayMethod=undefined&autoplay=false&userId=4673" width="775" height="650" name="Syndicated Content" frameborder="0"></iframe>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
     </>
-    $(document).ready(function(){$.getJSON('https://api.digitalmedia.hhs.gov/api/v2/resources/media/18240/syndicate.json?stripStyles=true&stripScripts=true&stripBreaks=false&stripImages=true&stripClasses=true&stripIds=true&displayMethod=undefined&autoplay=false&userId=4673&callback=?', function(data){$('#syndicatedContent_18240_227960618084237').html(data.results[0].content);});});
   )
 }
