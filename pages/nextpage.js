2e1264f3e5cd59461b6818f9a1ba404895653736
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-02 18:13:12 -0500

    more changes

--- pages/nextpage.js
+++ pages/nextpage.js
@@ -8,9 +8,6 @@ export default function nextpage() {
   return (
     <>
     <div>
-    <Bot />
-    </div>
-    <div>
     <Layout home>
       <Head>
         <title>Learn T1D</title>
@@ -60,6 +57,9 @@ export default function nextpage() {
 <strong>Syndicated Content Details:</strong><br />Source URL:&nbsp;<a href="https://www.niddk.nih.gov/health-information/diabetes/overview/what-is-diabetes">https://www.niddk.nih.gov/health-information/diabetes/overview/what-is-diabetes</a><br />Source Agency:&nbsp;<a href="http://www.niddk.nih.gov/">National Institute of Diabetes and Digestive and Kidney Diseases (NIDDK)</a><br />Captured Date: 2017-06-19 23:53:06.0
 </div>
     </Layout>
+    <div>
+    <Bot />
+    </div>
 </div>
 </>
   )
