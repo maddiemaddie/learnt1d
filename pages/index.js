017856d49c50e7442e03bc515f6c9a6f485cd280
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-02 18:54:24 -0500

    add bot

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Bot from '../components/bot';
 
 export default function Home() {
   return (
