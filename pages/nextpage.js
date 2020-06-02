5c6f66f827e4186936e60ca0d237d12c4ca400f9
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-02 18:30:23 -0500

    changing bot placement

--- pages/nextpage.js
+++ pages/nextpage.js
@@ -57,9 +57,9 @@ export default function nextpage() {
 <strong>Syndicated Content Details:</strong><br />Source URL:&nbsp;<a href="https://www.niddk.nih.gov/health-information/diabetes/overview/what-is-diabetes">https://www.niddk.nih.gov/health-information/diabetes/overview/what-is-diabetes</a><br />Source Agency:&nbsp;<a href="http://www.niddk.nih.gov/">National Institute of Diabetes and Digestive and Kidney Diseases (NIDDK)</a><br />Captured Date: 2017-06-19 23:53:06.0
 </div>
     </Layout>
-    <div>
-    <Bot />
-    </div>
+</div>
+<div>
+<Bot />
 </div>
 </>
   )
