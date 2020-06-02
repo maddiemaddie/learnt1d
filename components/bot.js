4c47c724f6a1ec6f34293712017477e5cc87505e
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-02 18:20:31 -0500

    theming

--- components/bot.js
+++ components/bot.js
@@ -1,35 +1,29 @@
 import ChatBot from 'react-simple-chatbot';
 
-function Bot() {
-  return (
-    <ChatBot
-    steps={[
-      {
-        id: '1',
-        message: 'What number I am thinking?',
-        trigger: '2',
-      },
-      {
-        id: '2',
-        options: [
-          { value: 1, label: 'Number 1', trigger: '4' },
-          { value: 2, label: 'Number 2', trigger: '3' },
-          { value: 3, label: 'Number 3', trigger: '3' },
-        ],
-      },
-      {
-        id: '3',
-        message: 'Wrong answer, try again.',
-        trigger: '2',
-      },
-      {
-        id: '4',
-        message: 'Awesome! You are a telepath!',
-        end: true,
-      },
-    ]}
-  />
-  )
-}
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#EF6C00',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#EF6C00',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
 
-export default Bot
+const steps = [
+  {
+    id: '1',
+    message: 'Hello World',
+    end: true,
+  },
+];
+
+const Bot = () => (
+  <ThemeProvider theme={theme}>
+    <ChatBot steps={steps} />;
+  </ThemeProvider>
+);
+
+export default Bot;
