e9d3e3750f03a14f1dae72d279576b6bd1a6a9e3
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-05 17:53:54 -0500

    break

--- components/layout.js
+++ components/layout.js
@@ -30,6 +30,7 @@ export default function Layout({ children, home }) {
             <Link href="/"><a><img src="logo.svg" alt="Learn T1D logo"  className="logo" width="100%" height="100%"/></a></Link>
             <ul className="spacer">
               <br />
+              <br />
               <li><a href="https://www.instagram.com/learnt1d/" target="_blank"><img src="instagram.svg" alt="Learn T1D logo"  className="logo" width="100%" height="100%"/></a></li>
             </ul>
           </>
