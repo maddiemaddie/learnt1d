e34acd82518502a9bdd5284ab1cf9b83619658fc
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-05 14:35:43 -0500

    float right

--- components/layout.js
+++ components/layout.js
@@ -29,7 +29,7 @@ export default function Layout({ children, home }) {
           <>
           <ul className="headbar">
             <li><img src="logo.svg" alt="Learn T1D logo" width="30%" height="30%" className="logo" /></li>
-            <li><img src="instagram.svg" alt="Instagram logo" width="5%" height="5%" className="insta" /></li>
+            <li><img src="instagram.svg" alt="Instagram logo" width="5%" height="5%" className="insta" style="float: right" /></li>
           </ul>
             <h1 className={utilStyles.heading2Xl}>learn t1d</h1>
           </>
