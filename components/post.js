7ca2e6f75d3975a3f0a7abfbc436a2d22dc0e860
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-05 20:57:29 -0500

    insta

--- components/post.js
+++ components/post.js
@@ -15,5 +15,102 @@ export default function Post() {
   onAfterRender={() => {}}
   onFailure={() => {}}
 />
+<></>
+<InstagramEmbed
+url='https://www.instagram.com/p/CA_b5mqB2u8/'
+maxWidth={320}
+hideCaption={true}
+containerTagName='div'
+injectScript
+protocol=''
+onLoading={() => {}}
+onSuccess={() => {}}
+onAfterRender={() => {}}
+onFailure={() => {}}
+/>
+<InstagramEmbed
+url='https://www.instagram.com/p/CBBesMBBOiz/'
+maxWidth={320}
+hideCaption={true}
+containerTagName='div'
+injectScript
+protocol=''
+onLoading={() => {}}
+onSuccess={() => {}}
+onAfterRender={() => {}}
+onFailure={() => {}}
+/>
+<InstagramEmbed
+url='https://www.instagram.com/p/CBBj-BsBCCh/'
+maxWidth={320}
+hideCaption={true}
+containerTagName='div'
+injectScript
+protocol=''
+onLoading={() => {}}
+onSuccess={() => {}}
+onAfterRender={() => {}}
+onFailure={() => {}}
+/>
+<InstagramEmbed
+url='https://www.instagram.com/p/CBBktyUhwkW/'
+maxWidth={320}
+hideCaption={true}
+containerTagName='div'
+injectScript
+protocol=''
+onLoading={() => {}}
+onSuccess={() => {}}
+onAfterRender={() => {}}
+onFailure={() => {}}
+/>
+<InstagramEmbed
+url='https://www.instagram.com/p/CBBpuPxhgj6/'
+maxWidth={320}
+hideCaption={true}
+containerTagName='div'
+injectScript
+protocol=''
+onLoading={() => {}}
+onSuccess={() => {}}
+onAfterRender={() => {}}
+onFailure={() => {}}
+/>
+<InstagramEmbed
+url='https://www.instagram.com/p/CBEDSvZBPGp/'
+maxWidth={320}
+hideCaption={true}
+containerTagName='div'
+injectScript
+protocol=''
+onLoading={() => {}}
+onSuccess={() => {}}
+onAfterRender={() => {}}
+onFailure={() => {}}
+/>
+<InstagramEmbed
+url='https://www.instagram.com/p/CBEGjaVhimT/'
+maxWidth={320}
+hideCaption={true}
+containerTagName='div'
+injectScript
+protocol=''
+onLoading={() => {}}
+onSuccess={() => {}}
+onAfterRender={() => {}}
+onFailure={() => {}}
+/>
+<InstagramEmbed
+url='https://www.instagram.com/p/CBEVEOsBzfC/'
+maxWidth={320}
+hideCaption={true}
+containerTagName='div'
+injectScript
+protocol=''
+onLoading={() => {}}
+onSuccess={() => {}}
+onAfterRender={() => {}}
+onFailure={() => {}}
+/>
 )
 }
