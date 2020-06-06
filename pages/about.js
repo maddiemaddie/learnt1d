2e58635c7d4ac253e3f4639cc487d370d5b74582
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-05 20:50:29 -0500

    go

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Bot from '../components/bot';
+import Post from '../components/post'
 
 export default function nextpage() {
   return (
