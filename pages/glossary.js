faddd502f13a2bb2df7e20673ff02cf109f75f0c
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-08 17:45:23 -0500

    Update glossary.js

--- pages/glossary.js
+++ pages/glossary.js
@@ -17,7 +17,15 @@ export default function nextpage() {
       <p>A hormone made by your pancreas that helps your body use glucose (blood sugar) for energy. Insulin helps to control blood glucose levels.</p>
       <h3>Pancreas -</h3>
       <p>An organ located behind the lower part of your stomach. The pancreas makes the hormone insulin to control blood glucose and also makes enzymes and fluids for digestion.</p>
-      <div class="reference">Defintions taken from the <a href="https://www.niddk.nih.gov/">National Institute of Diabetes and Digestive and Kidney Diseases</a></div>
+      <h3>Hormone</h3>
+      <p>A chemical one part of your body makes and releases into your blood to help trigger or control functions in another part of your body.</p>
+      <h3>Glucagon</h3>
+      <p>A hormone that raises blood glucose. Glucagon is given by a shot and is used to treat severe low blood sugar, or hypoglycemia.</p>
+      <h3>Immune system</h3>
+      <p>Your body's system for protecting itself from infection by finding and destroying bacteria, viruses, and other substances that may be harmful.</p>
+      <h3>A1C Test</h3>
+      <p>A blood test that provides information about your average levels of blood glucose, also called blood sugar, over the past 3 months.</p>
+  <div class="reference">Defintions taken from the <a href="https://www.niddk.nih.gov/">National Institute of Diabetes and Digestive and Kidney Diseases</a></div>
     </Layout>
   </div>
   <div className="bot">
