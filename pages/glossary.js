31c0098a3e7cb9ae617bbace76914f668246dc86
madtharp <55772921+madtharp@users.noreply.github.com>
2020-06-08 17:28:25 -0500

    glossary

--- pages/glossary.js
+++ pages/glossary.js
@@ -13,12 +13,11 @@ export default function nextpage() {
         <title>Learn T1D | Glossary</title>
       </Head>
       <h1>Glossary of Terms</h1>
-      <div className="defintion">
       <h3>Insulin -</h3>
       <p>A hormone made by your pancreas that helps your body use glucose (blood sugar) for energy. Insulin helps to control blood glucose levels.</p>
       <h3>Pancreas -</h3>
       <p>An organ located behind the lower part of your stomach. The pancreas makes the hormone insulin to control blood glucose and also makes enzymes and fluids for digestion.</p>
-      </div>
+      <div class="reference">Defintions taken from the National Institute of Diabetes and Digestive and Kidney Diseases</div>
     </Layout>
   </div>
   <div className="bot">
